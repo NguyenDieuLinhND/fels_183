f00c44004ec6367445bd11a5b598b0decb37e187
Linh <likxjk1402@gmail.com>
2016-09-08 12:34:38 +0700

    Admin create-update category

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,4 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 255},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: 1}
-
-  has_secure_password
 end
