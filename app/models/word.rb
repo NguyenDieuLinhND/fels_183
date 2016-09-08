ebab6bb6d6f0fbc8b355602853406d0a552b6ac5
Linh <likxjk1402@gmail.com>
2016-09-08 14:31:31 +0700

    Admin create word and answer

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,4 +3,10 @@ class Word < ApplicationRecord
 
   has_many :answers, dependent: :destroy
   has_many :results, dependent: :destroy
+
+  accepts_nested_attributes_for :answers,
+    reject_if: proc {|attribute| attribute[:answer_content].blank?},
+    allow_destroy: true
+
+  validates :name, presence: true
 end
