d8155b1c54197e0bd99e021b80a77a8e501a51e9
Linh <likxjk1402@gmail.com>
2016-09-14 09:46:58 +0700

    User see lesson list

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,9 @@ class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
       @word_learned = Word.learned current_user.id
+      @lessons = current_user.lessons.includes :category
+      @lessons = @lessons.order(updated_at: :desc).
+        paginate page: params[:page], per_page: Settings.size_lesson
     end
   end
 end
